7d3bb97549cb6c1c5cd75836746e2a0700fad34e
Alex <st10437348@imconnect.edu.za>
2025-11-21 10:41:29 +0200

    6th commit: I have addded the update program.cs code.

--- CMCSSummative/CMCSSummative/Program.cs
+++ CMCSSummative/CMCSSummative/Program.cs
@@ -1,36 +1,40 @@
-namespace CMCSSummative
+using CMCSSummative.Data;
+using CMCSSummative.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession(o => { o.Cookie.HttpOnly = true; o.IdleTimeout = TimeSpan.FromHours(4); });
+
+builder.Services.Configure<SecurityOptions>(builder.Configuration.GetSection("Security"));
+
+var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(conn));
+
+builder.Services.Configure<ApprovalRulesOptions>(builder.Configuration.GetSection("ApprovalRules"));
+
+builder.Services.AddScoped<ClaimValidationService>();
+
+builder.Services.AddSingleton<FileEncryptionService>();
+builder.Services.AddScoped<PdfReportService>();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapStaticAssets();
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
-
-            app.Run();
-        }
-    }
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
 }
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
